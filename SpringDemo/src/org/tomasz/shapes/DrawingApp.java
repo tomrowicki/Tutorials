b7efa6f911d8aa76ecacad5fcc9402555e38ea5b
Tomasz <Tomasz@Tomasz-PC.home>
2016-03-22 15:25:23 +0100

    Lesson 15 finished.

--- SpringDemo/src/org/tomasz/shapes/DrawingApp.java
+++ SpringDemo/src/org/tomasz/shapes/DrawingApp.java
@@ -3,6 +3,7 @@ package org.tomasz.shapes;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
@@ -13,7 +14,10 @@ public class DrawingApp {
 		//Triangle triangle = new Triangle();
 		
 		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml")); // simpler albeit less powerful way to do things
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); // more powerful and at the same time no more resource hungry way; requires spring.xml in the source folder!!!
+//		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); // more powerful and at the same time no more resource hungry way; requires spring.xml in the source folder!!!
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		// this allow for the registration of the moment when a bean is about to get destroyed
+		context.registerShutdownHook();
 		//Triangle triangle = (Triangle) factory.getBean("triangle"); // reference to spring.xml
 //		Triangle triangle = (Triangle) context.getBean("triangle-alias");
         Triangle triangle = (Triangle) context.getBean( "triangle1" );
