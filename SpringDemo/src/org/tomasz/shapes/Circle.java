b305823c8ec5703182b3e84daf19751364057275
Tomasz <Tomasz@Tomasz-PC.home>
2016-03-22 19:37:46 +0100

    Lesson 20 finished

--- SpringDemo/src/org/tomasz/shapes/Circle.java
+++ SpringDemo/src/org/tomasz/shapes/Circle.java
@@ -1,5 +1,9 @@
 package org.tomasz.shapes;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
@@ -26,10 +30,22 @@ public class Circle implements Shape {
 	// autowire the property
 	// to it; in the case of multiple objects it looks for the same id as the
 	// name of the field; requires AutowiredAnnotationBeanPostProcessor
-	@Autowired
+//	@Autowired
 	// this is used if the id/name check also fails to connect with the appropriate bean
-	@Qualifier("circleRelated")
+//	@Qualifier("circleRelated")
+	// another way to autowire properties
+	@Resource(name="pointC")
 	public void setCenter(Point center) {
 		this.center = center;
+	}
+	
+	@PostConstruct
+	public void initializeCircle() {
+		System.out.println("Init of Circle");
+	}
+	
+	@PreDestroy
+	public void destroyCircle() {
+		System.out.println("Destruction of Circle");
 	}
 }
